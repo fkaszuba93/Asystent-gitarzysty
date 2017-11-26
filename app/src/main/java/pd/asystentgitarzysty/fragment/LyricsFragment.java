ceded6525f9801e03c9c1633f7f932f63d0a70e0
Filip Kaszuba <fkaszuba93@gmail.com>
2017-11-26 15:06:52 +0100

    fragments layout; display lyrics

--- app/src/main/java/pd/asystentgitarzysty/fragment/LyricsFragment.java
+++ app/src/main/java/pd/asystentgitarzysty/fragment/LyricsFragment.java
@@ -1,11 +1,11 @@
 package pd.asystentgitarzysty.fragment;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import pd.asystentgitarzysty.R;
 
@@ -14,17 +14,29 @@ import pd.asystentgitarzysty.R;
  */
 public class LyricsFragment extends Fragment {
 
+    private TextView lyricsText, noLyricsText;
+    private View lyricsView;
 
     public LyricsFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lyrics, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_lyrics, container, false);
+        lyricsText = v.findViewById(R.id.lyrics_text);
+        noLyricsText = v.findViewById(R.id.no_lyrics_text);
+        lyricsView = v.findViewById(R.id.lyrics_view);
 
+        String l = "qwertyuiop\n" +
+                "asdfghjkl\n" +
+                "zxcvbnm\n";
+        lyricsText.setText(l);
+        lyricsView.setVisibility(View.VISIBLE);
+        noLyricsText.setVisibility(View.GONE);
+
+        return v;
+    }
 }
